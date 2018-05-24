3a1c5a52bffd1ba9c7d35c714c1cc4bf10f34051
Alexander Frolov <af@wono.io>
2018-05-24 15:28:05 +0300

    Исправил тестовые скрипты: получение аргументов

--- contracts/scripts/balance-of.js
+++ contracts/scripts/balance-of.js
@@ -1,20 +1,22 @@
 module.exports = function(callback) {
     try {
         let address;
-        if (!process.argv[4])
-            throw 'Bad or missing argument';
         if (process.argv[4].match(/^0x[0-9A-Fa-f]{40}$/))
             address = process.argv[4];
         else if (parseInt(process.argv[4], 10))
             address = web3.eth.accounts[parseInt(process.argv[4], 10)];
         else
             throw 'Bad or missing argument';
-        const Whitelist = artifacts.require("Whitelist");
-        const whitelist = Whitelist.at('0x0759939533e1f59e16c1fe7a2e736f0a3cf570b8');
-        whitelist.addAddress(address).then((result) => {console.log(result);});
+        const WonoToken = artifacts.require("WonoToken");
+        const token = WonoToken.at('0x7a4471267b797428a6a51cc73fbc9397710f4572');
+        token.balanceOf(address).then((result) => {console.log(web3.fromWei(result).toString());});
         callback();
     }
     catch(e) {
         callback(e);
     }
 }
+
+
+
+
--- contracts/scripts/remove-from-whitelist.js
+++ contracts/scripts/remove-from-whitelist.js
@@ -1,12 +1,17 @@
 module.exports = function(callback) {
     try {
+        let address;
+        if (!process.argv[4])
+            throw 'Bad or missing argument';
+        if (process.argv[4].match(/^0x[0-9A-Fa-f]{40}$/))
+            address = process.argv[4];
+        else if (parseInt(process.argv[4], 10))
+            address = web3.eth.accounts[parseInt(process.argv[4], 10)];
+        else
+            throw 'Bad or missing argument';
         const Whitelist = artifacts.require("Whitelist");
         const whitelist = Whitelist.at('0x0759939533e1f59e16c1fe7a2e736f0a3cf570b8');
-        whitelist.declineAddress(web3.eth.accounts[1]).then((receipt) => {console.log(receipt);});
-        whitelist.declineAddress(web3.eth.accounts[2]).then((receipt) => {console.log(receipt);});
-        whitelist.declineAddress(web3.eth.accounts[3]).then((receipt) => {console.log(receipt);});
-        whitelist.declineAddress(web3.eth.accounts[4]).then((receipt) => {console.log(receipt);});
-        whitelist.declineAddress(web3.eth.accounts[5]).then((receipt) => {console.log(receipt);});
+        whitelist.declineAddress(address).then((result) => {console.log(result);});
         callback();
     }
     catch(e) {
