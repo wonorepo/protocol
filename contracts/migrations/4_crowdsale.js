a8a778c42dbe4c0f97c5b0683fdf9fde42d9ed4c
Alexander Frolov <af@wono.io>
2018-06-02 02:22:07 +0300

    Workaround для бага в deployer()

--- contracts/migrations/4_crowdsale.js
+++ contracts/migrations/4_crowdsale.js
@@ -1,20 +1,11 @@
-const Crowdsale = artifacts.require("./Crowdsale.sol");
 const Whitelist = artifacts.require("./Whitelist.sol");
 const WonoToken = artifacts.require("./WonoToken.sol");
+const Crowdsale = artifacts.require("./Crowdsale.sol");
 
 module.exports = function(deployer) {
-    WonoToken.deployed()
-    .then(function(wonotoken) {
-        Whitelist.deployed()
-        .then(function(whitelist) {
-            let result = deployer.deploy(Crowdsale, whitelist.address, wonotoken.address, web3.eth.accounts[0]);
-            result.then(function(result) {
-                Crowdsale.deployed()
-                .then(function(crowdsale) {
-                    console.log(`\x1b[35;1m${crowdsale.address}\x1b[0m`);
-                });
-            });
-            return result;
-        });
-    });
+    let wonotoken;
+    WonoToken.deployed().then(function(addr) { wonotoken = addr; });
+    let whitelist;
+    Whitelist.deployed().then(function(addr) { whitelist = addr; });
+    deployer.deploy(Crowdsale, wonotoken, whitelist, web3.eth.accounts[0]);
 };
