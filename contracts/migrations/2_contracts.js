2b07ea8346c2f28792afb52dbfe9738ca4ca19b5
Alexander Frolov <af@wono.io>
2018-08-07 02:14:56 +0300

    Migration stuff for Kovan

--- contracts/migrations/2_contracts.js
+++ contracts/migrations/2_contracts.js
@@ -14,17 +14,17 @@ module.exports = function(deployer, network, accounts) {
         return deployer.deploy(Crowdsale, WonoToken.address, Whitelist.address);
     })
     .then(() => {
-        return deployer.deploy(TokenDistributor, Crowdsale.address);
+        return deployer.deploy(EtherDistributor, Crowdsale.address);
     })
     .then(() => {
-        return deployer.deploy(EtherDistributor, Crowdsale.address);
+        return deployer.deploy(TokenDistributor, Crowdsale.address);
     })
     .then(() => {
         console.log(`\x1b[36;1m${WonoToken.address}\x1b[0m`);
         console.log(`\x1b[32;1m${Whitelist.address}\x1b[0m`);
         console.log(`\x1b[35;1m${Crowdsale.address}\x1b[0m`);
-        console.log(`\x1b[33;1m${TokenDistributor.address}\x1b[0m`);
-        console.log(`\x1b[34;1m${EtherDistributor.address}\x1b[0m`);
+        console.log(`\x1b[33;1m${EtherDistributor.address}\x1b[0m`);
+        console.log(`\x1b[34;1m${TokenDistributor.address}\x1b[0m`);
     })
     .catch((e) => {
         console.error(`\x1b[31;1m${e}\x1b[0m`);
--- contracts/truffle.js
+++ contracts/truffle.js
@@ -3,14 +3,32 @@ module.exports = {
     development: {
       host: 'localhost',
       port: 8545,
-      network_id: '*',
-      gas: 5000000
-    }
+      network_id: 513,
+      gas: 5000000,
+      gasPrice: 2000000000,
+      from: '0x58bb9e15db607bcf3d1a9a78a5a71cb45adf18d5'
+    },
+    testnet: {
+      host: 'localhost',
+      port: 8545,
+      network_id: 8617,
+      gas: 80000000,
+      gasPrice: 2000000000,
+      from: '0xeD120005a06B5b8fb003BAb27B7a7f18f090caFe'
+    },
+    kovan: {
+      host: 'localhost',
+      port: 8545,
+      network_id: 42,
+      gas: 4700000,
+      gasPrice: 2000000000,
+      from: '0xeD120005a06B5b8fb003BAb27B7a7f18f090caFe'
+    }	    
   },
   solc: {
     optimizer: {
       enabled: true,
-      runs: 10
+      runs: 200
     }
   }
 }
