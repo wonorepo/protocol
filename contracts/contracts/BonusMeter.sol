aa4cd75ad470d3dd25f3440a561c474bdc85be82
Alexander Frolov <af@wono.io>
2018-08-17 14:36:45 +0300

    BonusMeter

---
+++ contracts/contracts/BonusMeter.sol
@@ -0,0 +1,32 @@
+pragma solidity ^0.4.23;
+
+import "./Crowdsale.sol";
+
+contract BonusMeter {
+    Crowdsale public crowdsale;
+    
+    constructor (address crowdsaleAddress) public {
+        crowdsale = Crowdsale(crowdsaleAddress);
+    }
+    
+    function getBonusPercent () public view returns (uint) {
+        uint totalCollected = crowdsale.totalCollected();
+        
+        if (totalCollected <= 1e24) {
+            return 70;
+        }
+        else if (totalCollected <= 4e24) {
+            return 30;
+        }
+        else if (totalCollected <= 8e24) {
+            return 25;
+        }
+        else if (totalCollected <= 9e24) {
+            return 10;
+        }
+        else if (totalCollected <= 10e14) {
+            return 5;
+        }
+        return 0;
+    }
+}
